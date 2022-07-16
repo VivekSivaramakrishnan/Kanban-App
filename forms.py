f108b61348653c2446f4614228eaa2c84f095633
Vivek Sivaramakrishnan <iamviveksrk@gmail.com>
2022-07-16 22:29:14 +0530

    Updated wtforms version. Made required changes.

--- forms.py
+++ forms.py
@@ -1,16 +1,18 @@
 from flask_wtf import FlaskForm
-from wtforms import TextField, PasswordField, TextAreaField
-from wtforms.validators import DataRequired, EqualTo, Length
+from wtforms import PasswordField, TextAreaField, SelectField, BooleanField, DateField
+from wtforms import StringField as TextField
+from wtforms.validators import DataRequired, EqualTo, Length, ValidationError
+from datetime import datetime
 
 # Set your classes here.
 
 
 class RegisterForm(FlaskForm):
     username = TextField(
-        'Username', validators=[DataRequired(), Length(min=2, max=25)]
+        'Username', validators=[DataRequired(), Length(min=1, max=25)]
     )
     password = PasswordField(
-        'Password', validators=[DataRequired(), Length(min=2, max=40)]
+        'Password', validators=[DataRequired(), Length(min=1, max=40)]
     )
     confirm = PasswordField(
         'Repeat Password',
@@ -24,5 +26,16 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', [DataRequired()])
 
 class ListForm(FlaskForm):
-    name = TextField('Name', [DataRequired(), Length(min=3, max=25)])
+    name = TextField('Name', [DataRequired(), Length(min=1, max=25)])
     description = TextAreaField('Description')
+
+class TaskForm(FlaskForm):
+    list = SelectField('List')
+    title = TextField('Title', [DataRequired(), Length(min=1, max=25)])
+    content = TextAreaField('Content')
+    deadline = DateField('Deadline')
+    status = BooleanField('Mark as complete')
+
+    def validate_deadline(form, field):
+        if field.data < datetime.date(datetime.now()):
+            raise ValidationError('Deadline must be not be earlier than today!')
